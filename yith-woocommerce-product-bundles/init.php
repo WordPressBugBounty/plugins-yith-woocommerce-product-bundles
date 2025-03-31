2ab82309adfb852ae38fe3b3d60e6b004998595f
Sean Murphy <sean@iamseanmurphy.com>
2025-03-31 12:19:21 +0000

    2.15.0

--- yith-woocommerce-product-bundles/init.php
+++ yith-woocommerce-product-bundles/init.php
@@ -3,17 +3,17 @@
  * Plugin Name: YITH WooCommerce Product Bundles
  * Plugin URI: https://yithemes.com/themes/plugins/yith-woocommerce-product-bundles
  * Description: <code><strong>YITH WooCommerce Product Bundles</strong></code> allows you to bundle WooCommerce products and sell them at a unique price. You can also set the quantity for each bundled item! <a href="https://yithemes.com/" target="_blank">Get more plugins for your e-commerce shop on <strong>YITH</strong></a>
- * Version: 2.14.0
+ * Version: 2.15.0
  * Author: YITH
  * Author URI: https://yithemes.com/
  * Text Domain: yith-woocommerce-product-bundles
  * Domain Path: /languages/
- * WC requires at least: 9.4
- * WC tested up to: 9.6.x
+ * WC requires at least: 9.6
+ * WC tested up to: 9.8.x
  *
  * @author  YITH <plugins@yithemes.com>
  * @package YITH WooCommerce Product Bundles
- * @version 2.14.0
+ * @version 2.15.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -51,7 +51,7 @@ register_activation_hook( __FILE__, 'yith_plugin_registration_hook' );
 
 
 if ( ! defined( 'YITH_WCPB_VERSION' ) ) {
-	define( 'YITH_WCPB_VERSION', '2.14.0' );
+	define( 'YITH_WCPB_VERSION', '2.15.0' );
 }
 
 if ( ! defined( 'YITH_WCPB_FREE_INIT' ) ) {
--- yith-woocommerce-product-bundles/readme.txt
+++ yith-woocommerce-product-bundles/readme.txt
@@ -2,9 +2,9 @@
 
 Contributors: yithemes
 Tags: product, bundle, bundles, product bundle, product bundles, bundle product, woocommerce, products, themes, yit, e-commerce, shop, yith, premium, yithemes
-Requires at least: 6.5
-Tested up to: 6.7
-Stable tag: 2.14.0
+Requires at least: 6.6
+Tested up to: 6.8
+Stable tag: 2.15.0
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -45,6 +45,11 @@ You can report security bugs through the Patchstack Vulnerability Disclosure Pro
 6. Admin view: Product bundle in the order
 
 == Changelog ==
+
+= 2.15.0 – Released on 31 March 2025 =
+
+* New: support for WordPress 6.8
+* New: support for WooCommerce 9.8
 
 = 2.14.0 – Released on 27 February 2025 =
 
